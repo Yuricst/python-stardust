3d5ed88ad019bf510634e2daefd2fe95eaa95698
Yuri <yuri.shimane@gmail.com>
2024-10-08 11:14:20 -0400

    tests updates

--- tests/test_twostage.py
+++ tests/test_twostage.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import stardust
 
 
-def test_twostage_cr3bp():
+def test_twostage_innerloop():
     # physical parameters
     LU = 389703     # km
     TU = 382981     # sec
@@ -48,11 +48,59 @@ def test_twostage_cr3bp():
     args = (mu, mu1, mu2)
     tspan = [0, 1.2*period_0]
     N = 20
+    prob = stardust.FixedTimeTwoStageOptimizer(
+        stardust.eom_stm_rotating_cr3bp,
+        rv0,
+        rvf,
+        tspan,
+        N = N,
+        args = args,
+    )
 
-    # # test inner loop
-    # print(f"Testing inner loop...")
-    # prob.create_nodes()
-    # sols = prob.inner_loop(prob.nodes[1:-1,0:3].flatten(), get_sols = True, maxiter = 10, verbose = True)
+    # test inner loop
+    print(f"Testing inner loop...")
+    prob.create_nodes()
+    sols = prob.inner_loop(prob.nodes[1:-1,0:3].flatten(), get_sols = True, maxiter = 10, verbose = True)
+    assert prob.inner_loop_success == True
+    return
+
+
+def test_outerloop_cr3bp():
+    # physical parameters
+    LU = 389703     # km
+    TU = 382981     # sec
+    MU = 1000.0     # kg
+
+    mu = 1.215058560962404e-02
+    mu1 = 1 - mu
+    mu2 = mu
+
+    # initial state
+    rv0 = np.array([1.0809931218390707E+00,
+          0.0000000000000000E+00,
+          -2.0235953267405354E-01,
+          1.0157158264396639E-14,
+          -1.9895001215078018E-01,
+          7.2218178975912707E-15])
+    period_0 = 2.3538670417546639E+00
+    sol0_ballistic = solve_ivp(stardust.eom_rotating_cr3bp, (0, period_0), rv0, args=(mu, mu1, mu2), 
+                               method='RK45', rtol=1e-12, atol=1e-12)
+
+    # final targeted state
+    rvf = np.array([1.1648780946517576,
+                    0.0,
+                    -1.1145303634437023E-1,
+                    0.0,
+                    -2.0191923237095796E-1,
+                    0.0])
+    period_f = 3.3031221822879884
+    solf_ballistic = solve_ivp(stardust.eom_rotating_cr3bp, (0, period_f), rvf, args=(mu, mu1, mu2),
+                               method='RK45', rtol=1e-12, atol=1e-12)
+
+    # construct problem
+    args = (mu, mu1, mu2)
+    tspan = [0, 1.2*period_0]
+    N = 20
 
     # test outer loop
     print(f"\nTesting outer loop in dense Jacobian mode...")
@@ -96,7 +144,7 @@ def test_twostage_cr3bp():
     ax.spy(prob.J_outer)
     ax.set_title('Jacobian Sparsity Pattern')
 
-    # export nodes to file 
+    # export nodes to file
     # np.savetxt(os.path.join(os.path.dirname(__file__), 'test_t_nodes.txt'), prob.times)
     # np.savetxt(os.path.join(os.path.dirname(__file__), 'test_nodes.txt'), prob.nodes)
     # np.savetxt(os.path.join(os.path.dirname(__file__), 'test_ubars.txt'), prob.v_residuals)
